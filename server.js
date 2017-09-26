4891ac9666c5c0a1e052084904ead8717baaf301
Trung Quan Nguyen <trquannguyen@techlove.vn>
2017-09-26 15:50:31 +0700

    change server.js

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
-var express = require('express'),
-  app = express(),
-  port = process.env.PORT || 8080;
+var express = require('express');
+var app = express();
 
-app.listen(port);
+console.log('RESTful API started on port: ' + port);
 
-console.log('todo list RESTful API server started on: ' + port);
+var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+});
+app.get('/', function (req, res) {
+    res.status(200).send('WELCOME TO BANANA');
+})
