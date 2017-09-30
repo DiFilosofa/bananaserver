b8034523cb89319d961ac773a69625331ab38f9e
Trung Quan Nguyen <trquannguyen@techlove.vn>
2017-09-30 14:36:39 +0700

    connect to server

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var app = express();
+var database = require('mongodb');
+var bodyParser = require('body-parser');
+var API_CONSTANT = "gYqqZQHORiIrJewS";
+var dbUrl = process.env.BANANA_DB_URL;
+var mongoClient = database.MongoClient;
 
 var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
@@ -8,3 +13,10 @@ var server = app.listen(process.env.PORT || 8080, function () {
 app.get('/', function (req, res) {
     res.status(200).send('WELCOME TO BANANA');
 })
+app.get('/test', function (req, res) {
+    res.status(200).send('testID');
+})
+
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://banana_admin:mrtiken@ds147864.mlab.com:47864/bananaserver');
