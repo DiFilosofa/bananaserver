de125cec7349df01112e3200f4b5bbc39eba024d
TQNg <ntq.quan0904@gmail.com>
2017-10-24 15:22:29 +0700

    update schema

--- api/TrafficEvent/Main/Points/TrafficEventPointModel.js
+++ api/TrafficEvent/Main/Points/TrafficEventPointModel.js
@@ -4,7 +4,7 @@ var Schema = mongoose.Schema;
 
 var trafficPointsSchema = new Schema({
     event_id:{
-        type:mongoose.Schema.Types.ObjectId,
+        type:Schema.ObjectId,
         ref:'TrafficEvent'
     },
     isVerified:{
